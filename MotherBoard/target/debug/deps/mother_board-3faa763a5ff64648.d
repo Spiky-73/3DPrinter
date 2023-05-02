2813634ac3c1bb6198258f2db7a37af0790b3fd0
Corentin <88098360+Spiky-73@users.noreply.github.com>
2023-05-02 09:09:53 +0200

    added vscode json files

--- MotherBoard/target/debug/deps/mother_board-3faa763a5ff64648.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cocoj\Documents\Perso\INSA\2A\P2I6\3DPrinter\MotherBoard\target\debug\deps\mother_board-3faa763a5ff64648.rmeta: src\main.rs
-
-C:\Users\cocoj\Documents\Perso\INSA\2A\P2I6\3DPrinter\MotherBoard\target\debug\deps\mother_board-3faa763a5ff64648.d: src\main.rs
-
-src\main.rs:
