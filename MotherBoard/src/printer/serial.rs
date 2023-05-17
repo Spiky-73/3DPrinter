25ac8de77c96a495d0c1e4531dfff5363117384f
Corentin <88098360+Spiky-73@users.noreply.github.com>
2023-05-17 16:39:21 +0200

    finishing the serial

--- MotherBoard/src/printer/serial.rs
+++ MotherBoard/src/printer/serial.rs
@@ -1,3 +1,4 @@
+use std::io::IoSlice;
 use std::{time::Duration, collections::HashMap};
 
 use std::thread;
@@ -5,19 +6,18 @@ use std::thread;
 use serialport::{self, SerialPort};
 
 
-
 impl Serial {
     
     pub fn new(port: &str, baud: u32) -> Self {
         Serial { 
-            port: serialport::new(port, baud)
-                .timeout(Duration::from_millis(10))
-                .open().expect("Failed to open port"), 
+            port: serialport::new(port, baud).timeout(Duration::from_millis(10)).open().expect("Failed to open port"), 
             handlers: HashMap::new(),
         }
     }
-
-    pub fn start_listenning(mut self){
+    
+    pub fn register_handler(&mut self, packet_id: u8, handler: fn(&[u8])) { self.handlers.insert(packet_id, handler); } 
+    
+    pub fn listen(&mut self) { 
         thread::spawn(move || {
             let mut buf = [0 as u8; 256];
             loop {
@@ -29,11 +29,20 @@ impl Serial {
             }
         });
     }
-    
-    pub fn register_handler(&mut self, packet_id: u8, handler: fn(&[u8])) {
-        self.handlers.insert(packet_id, handler);
-    }
 
+    pub fn send(&mut self, packetid: u8, data: Option<&[u8]>) {
+        match data {
+            Some(d) => {
+                let id = IoSlice::new(&[packetid]);
+                let data = IoSlice::new(&d);
+                self.port.write_vectored(&[id, data]);
+            }
+            None => {
+                self.port.write_all(&[packetid]);
+            }
+        }
+    }
+    
 }
 
 pub struct Serial {
